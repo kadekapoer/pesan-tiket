5645da7dc489526ebb08f2519aad4c85c2165f80
Arta Purniawan <kadeajus@gmail.com>
2022-10-09 22:55:48 +0800

    commit pertama

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    
+    <nav class="navbar bg-light">
+        <div class="container">
+          <a class="navbar-brand">Pesan Tiket</a>
+          <div class="nav-item d-flex">
+            <a class="nav-link me-2" href="#">Login</a>
+            <a class="nav-link" href="#">Register</a>
+          </div>
+        </div>
+      </nav>
+
+</x-app-layout>
